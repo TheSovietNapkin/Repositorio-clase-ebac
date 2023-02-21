42c0c3140d799a37f27ca6c97170f2b46c1ccc33
TheSovietNapkin <arijoseph2006@gmail.com>
2023-02-20 19:26:34 -0600

    Variables, String, Y chars

---
+++ Assets/Scripts/VariablesCharyString.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesCharyString : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        char c = '6';
+        int valorEntero = 0;
+        if (int.TryParse(c.ToString(), out valorEntero))
+        {
+            Debug.LogError("eso no es un tipo de dato valido");
+        }
+
+        Debug.Log(valorEntero);
+
+        char miCaracter;
+        string miString = "Hola desde EBAC";
+        string miSegundoString = miString.ToUpper();
+        string miTercerString = miString + " " + miSegundoString;
+        string ejemploEscpae = "c:\\Users\\Ari\\Documentos";
+        miCaracter = miString[13];
+        string miNombre = "Ari Joseph";
+        string misApellidos = "Seidel Martinez";
+        string salida = $"Mi nombre es: {miNombre}  mis Apellidos son {misApellidos}";
+        int longitud = miString.Length;
+        Debug.Log(salida);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/VariablesNumericas.cs
+++ Assets/Scripts/VariablesNumericas.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class VariablesNumericas : MonoBehaviour
 {
-    // Start is called before the first frame update
+    sbyte miBytConSigno = 0;
+    byte miByte = 0;
+    short miShort = 0;
+    ushort miShortSinSigno = 0;
+    int miInt = 3;
+    uint miIntSinSigno = 0;
+    long miLong = 0;
+    ulong miLongSinSigno = 0; 
+
     void Start()
     {
-        
+        int a, b, c;
+        float f; 
+        a = 10;
+        b = 5;
+        c = a + b;
+        Debug.Log(c);
+        c = a - b;
+        Debug.Log(c);
+        c = a * b;
+        Debug.Log(c);
+        f = a / b;
+        Debug.Log(c);
+
+        miInt = Random.RandomRange(a, b);
+        if (miInt % 2 == 0)
+        {
+            Debug.Log("El numero " + miInt + "es par");
+        }
+        else
+        {
+            Debug.Log("El numero " + miInt + "es non");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+
     }
 }
